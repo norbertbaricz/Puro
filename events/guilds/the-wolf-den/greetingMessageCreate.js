fd880d9255bdf04e1a643bef5ef663acd8420dbe
norbertbaricz <norbertbaricz608@gmail.com>
2025-10-07 23:26:33 +0300

    Modify and updated

--- events/guilds/the-wolf-den/greetingMessageCreate.js
+++ events/guilds/the-wolf-den/greetingMessageCreate.js
@@ -1,6 +1,8 @@
 const createGreetingEvent = require('../../../lib/guildGreetingEventFactory');
 
 module.exports = createGreetingEvent({
+    configPath: ['events', 'guilds', 'the-wolf-den', 'messageCreate'],
     allowedGuildIds: ['1217588804328620163'],
     allowedGuildNames: ['The Wolf Den'],
+    allowedGuilds: ['the-wolf-den'],
 });
