0c7fea564f913b373c2704550256c935a481809b
norbertbaricz <norbertbaricz608@gmail.com>
2025-10-07 23:40:52 +0300

    Optimization and preformance

--- events/guilds/masteras-animation-empire/messageCreate.js
+++ events/guilds/masteras-animation-empire/messageCreate.js
@@ -1,5 +1,8 @@
 const createGreetingEvent = require('../../../lib/guildGreetingEventFactory');
 
 module.exports = createGreetingEvent({
+    configPath: ['events', 'guilds', 'masteras-animation-empire', 'messageCreate'],
+    allowedGuildIds: ['1299378352301539398'],
     allowedGuildNames: ["Mastera's Animation Empire"],
+    allowedGuilds: ['masteras-animation-empire'],
 });
--- events/guilds/spencers-cave/messageCreate.js
+++ events/guilds/spencers-cave/messageCreate.js
@@ -1,5 +1,8 @@
 const createGreetingEvent = require('../../../lib/guildGreetingEventFactory');
 
 module.exports = createGreetingEvent({
+    configPath: ['events', 'guilds', 'spencers-cave', 'messageCreate'],
+    allowedGuildIds: ['1010638602004340787'],
     allowedGuildNames: ["Spencer's Cave"],
+    allowedGuilds: ['spencers-cave'],
 });
