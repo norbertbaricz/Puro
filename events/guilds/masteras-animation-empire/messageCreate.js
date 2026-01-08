66c6e1284a7ab02384ef8d7a97b9528b82da6631
norbertbaricz <norbertbaricz608@gmail.com>
2026-01-08 13:06:46 +0200

    refactor: Remove Mastera's Animation Empire commands and events; enhance logging error handling in wolfDenLogger

--- events/guilds/masteras-animation-empire/messageCreate.js
+++
@@ -1,8 +0,0 @@
-const createGreetingEvent = require('../../../lib/guildGreetingEventFactory');
-
-module.exports = createGreetingEvent({
-    configPath: ['events', 'guilds', 'masteras-animation-empire', 'messageCreate'],
-    allowedGuildIds: ['1299378352301539398'],
-    allowedGuildNames: ["Mastera's Animation Empire"],
-    allowedGuilds: ['masteras-animation-empire'],
-});
