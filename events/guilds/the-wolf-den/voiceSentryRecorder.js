e1a3b8293a00b7165a3f544d78a767ded06329b6
norbertbaricz <norbertbaricz608@gmail.com>
2026-01-08 09:22:38 +0200

    feat: Add premium guild logging configuration for the Wolf Den refactor: Remove deprecated voice recording event handlers refactor: Update logging functions to utilize new configuration chore: Update package-lock.json with new dependencies

--- events/guilds/the-wolf-den/voiceSentryRecorder.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-const { handleVoiceStateUpdate } = require('../../../lib/wolfDenRecorder');
-
-module.exports = {
-  name: Events.VoiceStateUpdate,
-  async execute(oldState, newState, client) {
-    await handleVoiceStateUpdate(oldState, newState, client);
-  },
-};
