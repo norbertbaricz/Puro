04ab8a37206107c5c3e62f2a6930e7d0dde598f1
norbertbaricz <norbertbaricz608@gmail.com>
2026-01-31 00:31:36 +0200

    deleted howl-greeting.js and greetingMessageCreate.js from the-wolf-den

--- events/guilds/the-wolf-den/greetingMessageCreate.js
+++
@@ -1,8 +0,0 @@
-const createGreetingEvent = require('../../../lib/guildGreetingEventFactory');
-
-module.exports = createGreetingEvent({
-    configPath: ['events', 'guilds', 'the-lunar-pack', 'messageCreate'],
-    allowedGuildIds: ['1217588804328620163'],
-    allowedGuildNames: ['The Lunar Pack'],
-    allowedGuilds: ['the-lunar-pack'],
-});
